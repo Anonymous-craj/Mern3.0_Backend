0f028d66aa97ef4c05f4572978cb402b1d1b8de6
Anonymous-craj <sirajbasnet5@gmail.com>
2025-09-01 21:41:57 +0545

    added model

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const connectToDB = require("./database");
 const app = express();
+
+app.use(express.json());
+
+const connectToDB = require("./database");
 
 app.get("/", (req, res) => {
   res.json({
@@ -9,9 +12,10 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/about", (req, res) => {
-  res.json({
-    message: "This is about page!",
+app.post("/blog", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    message: "Blog api triggered!",
   });
 });
 
